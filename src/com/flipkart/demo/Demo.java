a402616d9e82398229f57191151e6de5c6c512fd
Parth Khanna <parth.khanna@BLRETV-Q4292XQF.local>
2022-06-14 17:22:29 +0530

    Intial commit

---
+++ src/com/flipkart/demo/Demo.java
@@ -0,0 +1,11 @@
+package com.flipkart.demo;
+
+public class Demo {
+    public static void main(String[] args) {
+
+        // Creates a reader instance which takes
+        // input from standard input - keyboard
+
+        System.out.println("You entered: " );
+    }
+}
